fb595cd6c858e24a0c05caf764a7c22d0e0a4802
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2022-05-16 16:48:50 +0900

    Update DimacsEncoder formatter

--- lib/ravensat/dimacs_encoder.rb
+++ lib/ravensat/dimacs_encoder.rb
@@ -8,7 +8,7 @@ module Ravensat
     def to_dimacs(formula)
       return nil unless formula.cnf?
 
-      dimacs_header = "p cnf #{formula.vars_size} #{formula.clauses_size}"
+      dimacs_header = "p cnf #{formula.vars_size} #{formula.clauses_size}\n"
       dimacs_body = ""
       create_table(formula)
       formula.each do |node|
@@ -20,6 +20,8 @@ module Ravensat
         end
       end
 
+      # dimacs_body formatting
+      dimacs_body.strip! << ' '
       dimacs_body.gsub!(/\n{2,}/, "\n").gsub!(/\n/, "0\n") << '0'
 
       dimacs_header + dimacs_body
