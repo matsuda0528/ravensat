fc33045b187517199cf0beb0dbd6f5ae0056cd26
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2022-05-18 17:38:31 +0900

    Add all_different method to RavenClaw

--- lib/ravensat/ravenclaw.rb
+++ lib/ravensat/ravenclaw.rb
@@ -1,14 +1,18 @@
 module Ravensat
-  class RavenClaw
-    def initialize; end
-
-    def self.alo(prop_vars)
-      prop_vars.reduce(:|)
+  module RavenClaw
+    def self.alo(bool_vars)
+      bool_vars.reduce(:|)
     end
 
-    def self.amo(prop_vars)
-      prop_vars.combination(2).map do |e|
+    def self.amo(bool_vars)
+      bool_vars.combination(2).map do |e|
         e.map(&:~@).reduce(:|)
+      end.reduce(:&)
+    end
+
+    def self.all_different(*int_vars)
+      int_vars.combination(2).map do |int_var|
+        int_var.reduce(:!=)
       end.reduce(:&)
     end
   end
