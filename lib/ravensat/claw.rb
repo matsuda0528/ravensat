7a6da8117c5c007b6923796bcb54f4b699c424d9
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2022-06-15 15:27:49 +0900

    Add commander encoding

--- lib/ravensat/claw.rb
+++ lib/ravensat/claw.rb
@@ -4,10 +4,29 @@ module Ravensat
       bool_vars.reduce(:|)
     end
 
-    def self.amo(bool_vars)
+    def self.pairwise_amo(bool_vars)
       bool_vars.combination(2).map do |e|
         e.map(&:~@).reduce(:|)
       end.reduce(:&)
+    end
+
+    def self.commander_amo(bool_vars)
+      m = bool_vars.size / 2
+      introductory_variables = []
+      formula = Ravensat::InitialNode.new
+      bool_vars.each_slice(2) do |e|
+        c = Ravensat::VarNode.new
+        subset = e << ~c
+        formula &= pairwise_amo(subset)
+        formula &= alo(subset)
+        introductory_variables << c
+      end
+
+      if m < 6
+        formula &= pairwise_amo(introductory_variables)
+      else
+        formula &= commander_amo(introductory_variables)
+      end
     end
 
     def self.all_different(*int_vars)
@@ -19,5 +38,7 @@ module Ravensat
     def self.all_only_one(*int_vars)
       int_vars.map(&:only_one).reduce(:&)
     end
+
+    # alias :amo :pairwise_amo
   end
 end
