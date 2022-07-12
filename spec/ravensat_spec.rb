8f6e9d3ba6d06657a1ae931ca2baf8be4a84e1eb
rikuto matsuda <p99y92og@s.okayama-u.ac.jp>
2022-07-12 15:33:30 +0900

    Update spec to add unit test for to_dimacs method

--- spec/ravensat_spec.rb
+++ spec/ravensat_spec.rb
@@ -2,17 +2,10 @@
 
 RSpec.describe Ravensat do
   let(:solver) { Ravensat::Solver.new }
+  let(:encoder) {Ravensat::DimacsEncoder.new}
   let(:p1) { Ravensat::VarNode.new }
   let(:p2) { Ravensat::VarNode.new }
   let(:p3) { Ravensat::VarNode.new }
-
-  it "has a version number" do
-    expect(Ravensat::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(false)
-  end
 
   it "solves for CNF-SAT" do
     formula = ~p1 & (p1 | p2) & (p1 | p3)
@@ -35,5 +28,10 @@ RSpec.describe Ravensat do
   it "determines CNF" do
     formula = p1 & p2 | p3 | ~p1 & ~p2 | ~p3
     expect(formula.cnf?).to eq false
+  end
+
+  it "generates DIMACS Format" do
+    formula = (p1 | p2 | ~p3) & (~p1 | ~p2) & p1 & p3
+    expext(encoder.to_dimacs formula).to eq "p cnf 3 4\n1 2 -3 0\n-1 -2 0\n1 0\n3 0"
   end
 end
