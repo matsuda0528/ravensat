7c49bc4687d6b1727466f9e2ebcee8f7cac3705c
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2022-08-23 14:05:48 +0900

    Update Node to add exception

--- lib/ravensat/ast/initial_node.rb
+++ lib/ravensat/ast/initial_node.rb
@@ -1,10 +1,12 @@
 module Ravensat
   class InitialNode < Node
     def &(object)
+      raise TypeError.new("#{object.class} can't be coerced into Ravensat::Node") unless object.is_a? Node
       object
     end
 
     def |(object)
+      raise TypeError.new("#{object.class} can't be coerced into Ravensat::Node") unless object.is_a? Node
       object
     end
   end
